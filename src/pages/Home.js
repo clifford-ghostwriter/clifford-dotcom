c6e04064c2b2a5f76669cd9fdb68c0102a92af2f
clifford-ghostwriter <cliffizuchucks@gmail.com>
2024-03-26 20:28:48 +0100

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import cliffordOne from "../assets/cliffordone.jpg";
+import cliffordTwo from "../assets/cliffordtwo.jpg";
+
 const Home = () => {
   return <div>Home alone</div>;
 };
