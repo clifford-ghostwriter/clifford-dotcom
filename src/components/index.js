f4a357fb47f4e8fd7a6797e6778c7d83c1c92402
clifford-ghostwriter <cliffizuchucks@gmail.com>
2024-09-20 20:50:23 +0100

    name update again

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import Image from "./image";
 import Loading from "./loading";
 import Navbar from "./navbar";
 import Project from "./project";
-import Projects from "./Projects";
+import Projects from "./projects";
 import Sidebar from "./sidebar";
 import Technology from "./Technology";
 
